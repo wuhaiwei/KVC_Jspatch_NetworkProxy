3d55bb14733db7cbd54114d2e6503f104f4dbf4e
wu <wuhaiwei@wanglibank.com>
2016-11-28 17:31:00 +0800

    Initial Commit

---
+++ KVCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVCDemo
+//
+//  Created by  wuhiwi on 16/11/28.
+//  Copyright © 2016年 wanglibank.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KVCDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KVCDemo
+//
+//  Created by  wuhiwi on 16/11/28.
+//  Copyright © 2016年 wanglibank.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
